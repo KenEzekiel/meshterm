a1e36f1baf3956a14373c80cdefefbebe9ad1db7
KenEzekiel <kenneth.ezekiel.suprantoni@xendit.co>
2026-04-30 19:54:25 +0700

    [Chore] Bump to 0.15.0

--- packages/cli/index.ts
+++ packages/cli/index.ts
@@ -276,7 +276,7 @@ if (args.version) {
     const pkg = JSON.parse(readFileSync(join(import.meta.dir, "../../package.json"), "utf-8"));
     console.log(`meshterm v${pkg.version}`);
   } catch {
-    console.log("meshterm v0.14.0");
+    console.log("meshterm v0.15.0");
   }
   process.exit(0);
 }
